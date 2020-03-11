a638d5efa47682b2d4528929be4dbbb7fa68a4ea
Colthan <avecolth@gmail.com>
2020-03-11 11:32:45 -0700

    first push

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spawnRate = 0.5f;
+    private float timestamp = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= timestamp)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timestamp = Time.time + spawnRate;
+        }
+    }
+}
